290e6c0af9bd73b8684575b50fd709433b97d98a
Harsha Kethineni <harshakethineni@flywheel.io>
2021-09-07 10:26:27 -0500

    get tracer

--- starlette_opentracing/middleware.py
+++ starlette_opentracing/middleware.py
@@ -8,10 +8,10 @@ from starlette.requests import Request
 
 
 class StarletteTracingMiddleWare(BaseHTTPMiddleware):
-    def __init__(self, app, tracer, use_template: bool = False):
+    def __init__(self, app, get_tracer, use_template: bool = False):
         # Todo: add choice between global tracer and tracer that is already configured
         super().__init__(app)
-        self._tracer = tracer
+        self.get_tracer = get_tracer
         self.use_template = use_template
 
     def get_template(self, request: Request) -> str:
@@ -31,18 +31,19 @@ class StarletteTracingMiddleWare(BaseHTTPMiddleware):
         return method_path
 
     async def dispatch(self, request, call_next):
+        tracer = self.get_tracer()
         span_ctx = None
         headers = {}
         for k, v in request.headers.items():
             headers[k.lower()] = v
 
         try:
-            span_ctx = self._tracer.extract(opentracing.Format.HTTP_HEADERS, headers)
+            span_ctx = tracer.extract(opentracing.Format.HTTP_HEADERS, headers)
         except (InvalidCarrierException, SpanContextCorruptedException):
             pass
 
         scope = request.scope
-        with self._tracer.start_active_span(
+        with tracer.start_active_span(
             str(scope["path"]), child_of=span_ctx, finish_on_close=True
         ) as tracing_scope:
             span = tracing_scope.span
@@ -51,7 +52,14 @@ class StarletteTracingMiddleWare(BaseHTTPMiddleware):
             span.set_tag(tags.HTTP_METHOD, scope["method"])
             host, port = scope["server"]
             url = urlunparse(
-                (str(scope["scheme"]), f"{host}:{port}", str(scope["path"]), "", str(scope["query_string"]), "",)
+                (
+                    str(scope["scheme"]),
+                    f"{host}:{port}",
+                    str(scope["path"]),
+                    "",
+                    str(scope["query_string"]),
+                    "",
+                )
             )
             span.set_tag(tags.HTTP_URL, url)
 
